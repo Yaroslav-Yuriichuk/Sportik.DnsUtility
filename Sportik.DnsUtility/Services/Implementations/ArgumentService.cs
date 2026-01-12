8b799103db6e92142d8ae5dd83dc950a4066d04a
Yaroslav <mr.sluvik822@gmail.com>
2026-01-12 09:29:55 +0100

    Add an option to specify host name in the arguments

--- Sportik.DnsUtility/Services/Implementations/ArgumentService.cs
+++ Sportik.DnsUtility/Services/Implementations/ArgumentService.cs
@@ -4,12 +4,16 @@ using Sportik.DnsUtility.Services.Interfaces;
 
 namespace Sportik.DnsUtility.Services.Implementations;
 
-public class IpAddressArgumentService(IConfiguration configuration) : IIpAddressArgumentService
+public class ArgumentService(IConfiguration configuration) : IArgumentService
 {
     private const string AddressArgument = "--addr";
+    private const string HostNameArgument = "--host";
 
     private readonly string _defaultIpAddress = configuration["Dns:DefaultIpAddress"] ??
                                                 throw new ArgumentNullException(nameof(configuration), "Default IP address configuration is missing.");
+
+    private readonly string _defaultHostName = configuration["Dns:HostName"] ??
+                                               throw new ArgumentNullException(nameof(configuration), "Default host name configuration is missing.");
 
     public bool TryParseIpAddress(string[] args, out IPAddress? ipAddress)
     {
@@ -25,5 +29,21 @@ public class IpAddressArgumentService(IConfiguration configuration) : IIpAddress
         }
 
         return IPAddress.TryParse(ipString, out ipAddress);
+    }
+
+    public bool TryParseHostName(string[] args, out string? hostName)
+    {
+        hostName = _defaultHostName;
+
+        for (int i = 0; i < args.Length; i++)
+        {
+            if (args[i] == HostNameArgument && i + 1 < args.Length)
+            {
+                hostName = args[i + 1].Trim('"', '\'');
+                break;
+            }
+        }
+
+        return !string.IsNullOrWhiteSpace(hostName);
     }
 }
--- Sportik.DnsUtility/Services/Interfaces/IArgumentService.cs
+++ Sportik.DnsUtility/Services/Interfaces/IArgumentService.cs
@@ -3,9 +3,9 @@ using System.Net;
 namespace Sportik.DnsUtility.Services.Interfaces;
 
 /// <summary>
-/// Interface for server parsing command line arguments.
+/// Interface for parsing command line arguments.
 /// </summary>
-public interface IIpAddressArgumentService
+public interface IArgumentService
 {
     /// <summary>
     /// Parses the IP address from command line arguments.
@@ -14,4 +14,12 @@ public interface IIpAddressArgumentService
     /// <param name="ipAddress">The parsed IP address.</param>
     /// <returns>True if an IP address was successfully parsed; otherwise, false.</returns>
     bool TryParseIpAddress(string[] args, out IPAddress? ipAddress);
+
+    /// <summary>
+    /// Parses the host name from command line arguments.
+    /// </summary>
+    /// <param name="args">Command line arguments.</param>
+    /// <param name="hostName">The parsed host name.</param>
+    /// <returns>True if a host name was successfully parsed; otherwise, false.</returns>
+    bool TryParseHostName(string[] args, out string? hostName);
 }
